ef4e6e563961f6b167ac771b8361f94c4e01b57c
seoye0ng <bsy0313@gmail.com>
2023-11-13 23:39:17 +0900

    Test: event 혜택 중 평일 할인 테스트 코드 작성

--- __tests__/EventTest.js
+++ __tests__/EventTest.js
@@ -14,4 +14,18 @@ describe('이벤트 혜택 기능 테스트', () => {
     expect(christmas).toBe(1500);
     expect(christmas2).toBe(0);
   });
+
+  test('평일 할인 혜택을 반환하는 기능 테스트', () => {
+    // given
+    const menus = { 해산물파스타: '1', 초코케이크: '2' };
+    const menus2 = { 양송이수프: '1', 바비큐립: '1' };
+
+    // when
+    const weekday = Event.benefit.weekday(menus);
+    const weekday2 = Event.benefit.weekday(menus2);
+
+    // then
+    expect(weekday).toBe(4046);
+    expect(weekday2).toBe(0);
+  });
 });
