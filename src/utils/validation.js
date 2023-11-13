27de269b5bc0229443d755aa2a2a03429e998f43
seoye0ng <bsy0313@gmail.com>
2023-11-14 00:01:35 +0900

    Feat: 식당 날짜 유효성 검사시 숫자가 아닐 경우 에러 처리 추가

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,9 +1,11 @@
 function throwValidationError(message) {
-  throw new Error(`[ERROR] ${message}. 다시 입력해 주세요.`);
+  throw new Error(`[ERROR] ${message} 다시 입력해 주세요.`);
 }
 
 export function validateDate(date) {
   if (Number(date) < 1 || Number(date) > 31)
+    throwValidationError('유효하지 않은 날짜입니다.');
+  if (Number.isNaN(Number(date)))
     throwValidationError('유효하지 않은 날짜입니다.');
 
   return date;
