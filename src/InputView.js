40bfbb3d68c4db33ba568663ec5fd992a716f0c4
seoye0ng <bsy0313@gmail.com>
2023-11-13 17:10:50 +0900

    Feat: 식당 날짜를 입력받고 유효성 검사하는 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,7 +1,13 @@
-export default InputView = {
-    async readDate() {
-        const input = await Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        // ...
-    }
-    // ...
-}
+import { Console } from '@woowacourse/mission-utils';
+import { validateDate } from './utils/validation';
+
+const InputView = {
+  async readDate() {
+    const date = await Console.readLineAsync(
+      '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
+    );
+    return validateDate(date);
+  },
+};
+
+export default InputView;
---
+++ src/utils/validation.js
@@ -0,0 +1,10 @@
+function throwValidationError(message) {
+  throw new Error(`[ERROR] ${message}. 다시 입력해 주세요.`);
+}
+
+export function validateDate(date) {
+  if (Number(date) < 1 || Number(date) > 31)
+    throwValidationError('유효하지 않은 날짜입니다.');
+
+  return date;
+}
