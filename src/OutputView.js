6c362737ec3e68fbfcd807f69874620e18cb7283
seoye0ng <bsy0313@gmail.com>
2023-11-15 00:14:05 +0900

    Feat: 혜택 내역을 출력하는 기능 구현

--- src/OutputView.js
+++ src/OutputView.js
@@ -17,6 +17,21 @@ const OutputView = {
   printGiftMenu(price) {
     Console.print(price !== 0 ? Number(price).toLocaleString() : '없음');
   },
+
+  // 혜택 내역
+  printBenefitList(benefitList) {
+    const isEmptyBenefit = Object.values(benefitList).every((benefit) => {
+      return benefit === 0;
+    });
+
+    if (isEmptyBenefit) Console.print('없음');
+    else
+      Object.entries(benefitList)
+        .filter(([key, value]) => value !== 0)
+        .forEach(([key, value]) => {
+          Console.print(`${key}: -${Number(value).toLocaleString()}원`);
+        });
+  },
 };
 
 export default OutputView;
