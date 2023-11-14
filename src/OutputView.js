d5777e183390377e0366a42c775bc036240dc004
seoye0ng <bsy0313@gmail.com>
2023-11-15 00:48:56 +0900

    Refactor: Output 객체에서 사용하는 상수 모듈화

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,5 +1,9 @@
 import { Console } from '@woowacourse/mission-utils';
-import { CONTENT_TYPE } from './constants/OutputViewConstant.js';
+import {
+  CONTENT_TYPE,
+  EMPTY_VALUE,
+  MONETARY_UNIT,
+} from './constants/OutputViewConstant.js';
 
 const OutputView = {
   printGuideMessage(message) {
@@ -39,12 +43,12 @@ const OutputView = {
 
   // 할인 전 총주문 금액, 할인 후 예상 결제 금액
   printTotalOrderPrice(price) {
-    Console.print(`${Number(price).toLocaleString()}원`);
+    Console.print(`${Number(price).toLocaleString()}${MONETARY_UNIT}`);
   },
 
   // 증정 메뉴
   printGiftMenu(price) {
-    Console.print(price !== 0 ? Number(price).toLocaleString() : '없음');
+    Console.print(price !== 0 ? Number(price).toLocaleString() : EMPTY_VALUE);
   },
 
   // 혜택 내역
@@ -53,23 +57,25 @@ const OutputView = {
       return benefit === 0;
     });
 
-    if (isEmptyBenefit) Console.print('없음');
+    if (isEmptyBenefit) Console.print(EMPTY_VALUE);
     else
       Object.entries(benefitList)
         .filter(([key, value]) => value !== 0)
         .forEach(([key, value]) => {
-          Console.print(`${key}: -${Number(value).toLocaleString()}원`);
+          Console.print(
+            `${key}: -${Number(value).toLocaleString()}${MONETARY_UNIT}`,
+          );
         });
   },
 
   // 총혜택 금액
   printBenefitPrice(price) {
-    Console.print(`-${Number(price).toLocaleString()}원`);
+    Console.print(`-${Number(price).toLocaleString()}${MONETARY_UNIT}`);
   },
 
   // 12월 이벤트 배지
   printEventBadge(badge) {
-    Console.print(badge !== null ? badge : '없음');
+    Console.print(badge !== null ? badge : EMPTY_VALUE);
   },
 };
 
---
+++ src/constants/OutputViewConstant.js
@@ -0,0 +1,17 @@
+export const PRINT_MESSAGE = (date) => ({
+  greeting: '안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.',
+  preview: `12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기`,
+});
+
+export const CONTENT_TYPE = {
+  orderMenu: '<주문 메뉴>',
+  totalOrderPrice: '<할인 전 총주문 금액>',
+  giftMenu: '<증정 메뉴>',
+  benefitList: '<혜택 내역>',
+  benefitPrice: '<총혜택 금액>',
+  discountedTotalOrderPrice: '<할인 후 예상 결제 금액>',
+  eventBadge: '<12월 이벤트 배지>',
+};
+
+export const EMPTY_VALUE = '없음';
+export const MONETARY_UNIT = '원';
