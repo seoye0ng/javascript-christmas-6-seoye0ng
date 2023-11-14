427e8fd98be2500a87d9cc35d18f8ed09b0a3833
seoye0ng <bsy0313@gmail.com>
2023-11-14 18:51:06 +0900

    Feat: 사용자가 잘못된 값을 입력한 경우 에러 메시지를 출력하고 재입력받는 기능 구현

--- src/InputView.js
+++ src/InputView.js
@@ -3,18 +3,28 @@ import { validateDate, validateMenu } from './utils/validation';
 
 const InputView = {
   async readDate() {
-    const date = await Console.readLineAsync(
-      '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
-    );
-    return validateDate(date);
+    try {
+      const date = await Console.readLineAsync(
+        '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
+      );
+      return validateDate(date);
+    } catch (error) {
+      Console.print(error.message);
+      return this.readDate(); // 재귀적으로 함수 호출
+    }
   },
 
   async readMenu() {
-    const input = await Console.readLineAsync(
-      '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
-    );
-    const orderMenus = input.split(',');
-    return validateMenu(orderMenus);
+    try {
+      const input = await Console.readLineAsync(
+        '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
+      );
+      const orderMenus = input.split(',');
+      return validateMenu(orderMenus);
+    } catch (error) {
+      Console.print(error.message);
+      return this.readMenu();
+    }
   },
 };
 export default InputView;
