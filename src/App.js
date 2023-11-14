a65531dcbfd0b37ac988eeefd5c9719a93cf2c02
seoye0ng <bsy0313@gmail.com>
2023-11-14 19:47:27 +0900

    Feat: 날짜와 메뉴 받아와서 저장하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import InputView from './InputView.js';
+
 class App {
-  async run() {}
+  async run() {
+    const date = await InputView.readDate();
+    const orderMenuList = await InputView.readMenu();
+  }
 }
 
 export default App;
