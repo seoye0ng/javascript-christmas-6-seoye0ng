ebd76576a48db511e0e765382cc868fb9d05e4fc
seoye0ng <bsy0313@gmail.com>
2023-11-15 18:26:19 +0900

    Refactor: Input 객체에서 사용하는 상수 모듈화

--- src/InputView.js
+++ src/InputView.js
@@ -1,12 +1,11 @@
 import { Console } from '@woowacourse/mission-utils';
 import { validateDate, validateMenu } from './utils/validation';
+import { REQUEST_MESSAGE } from './constants/InputViewConstant.js';
 
 const InputView = {
   async readDate() {
     try {
-      const date = await Console.readLineAsync(
-        '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
-      );
+      const date = await Console.readLineAsync(REQUEST_MESSAGE.date);
       return validateDate(date);
     } catch (error) {
       Console.print(error.message);
@@ -16,9 +15,7 @@ const InputView = {
 
   async readMenu() {
     try {
-      const input = await Console.readLineAsync(
-        '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
-      );
+      const input = await Console.readLineAsync(REQUEST_MESSAGE.menu);
       const orderMenus = input.split(',');
       return validateMenu(orderMenus);
     } catch (error) {
---
+++ src/constants/InputViewConstant.js
@@ -0,0 +1,10 @@
+export const REQUEST_MESSAGE = {
+  date: '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
+  menu: '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
+};
+
+export const ERROR_MESSAGE = {
+  AGAIN: '다시 입력해 주세요.',
+  INVALID_DATE: '[ERROR] 유효하지 않은 날짜입니다.',
+  INVALID_ORDER: '[ERROR] 유효하지 않은 주문입니다.',
+};
