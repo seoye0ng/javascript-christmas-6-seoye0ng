86e08db3fcb19201704cbb09ac5feb36707b08b6
seoye0ng <bsy0313@gmail.com>
2023-11-15 19:27:00 +0900

    Feat: Event객체에 benefitType객체 추가

--- src/Event.js
+++ src/Event.js
@@ -25,6 +25,14 @@ const Event = {
     },
   },
 
+  benefitType: {
+    christmas: '크리스마스 디데이 할인',
+    weekday: '평일 할인',
+    weekend: '주말 할인',
+    special: '특별 할인',
+    gift: '증정 이벤트',
+  },
+
   benefit: {
     christmas(date) {
       if (Number(date) <= Event.period.christmas) {
