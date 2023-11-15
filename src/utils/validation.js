bd93a7371f4e52651b03d8827b783a7f6c47a4ba
seoye0ng <bsy0313@gmail.com>
2023-11-15 18:44:56 +0900

    Refactor: input validation 함수 분리 및 리팩토링

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,64 +1,71 @@
 import Event from '../Event';
+import { ERROR_MESSAGE } from '../constants/InputViewConstant.js';
 
 function throwValidationError(message) {
-  throw new Error(`[ERROR] ${message} 다시 입력해 주세요.`);
+  throw new Error(`${message} ${ERROR_MESSAGE.AGAIN}`);
 }
 
 export function validateDate(date) {
-  if (Number(date) < 1 || Number(date) > 31)
-    throwValidationError('유효하지 않은 날짜입니다.');
-  if (Number.isNaN(Number(date)))
-    throwValidationError('유효하지 않은 날짜입니다.');
+  const parsedDate = Number(date);
+  if (parsedDate < 1 || parsedDate > 31 || Number.isNaN(parsedDate))
+    throwValidationError(ERROR_MESSAGE.INVALID_DATE);
 
   return date;
 }
 
-function isValidMenuName(name, validMenus) {
-  return validMenus.includes(name);
+function isValidMenuName(name) {
+  const eventMenus = [
+    ...Object.keys(Event.menu.appetizer),
+    ...Object.keys(Event.menu.main),
+    ...Object.keys(Event.menu.dessert),
+    ...Object.keys(Event.menu.drink),
+  ];
+
+  return eventMenus.includes(name);
+}
+
+function transformOrderMenuFormat(menu) {
+  if (!menu.includes('-')) throw new Error(ERROR_MESSAGE.INVALID_ORDER);
+
+  const [name, quantity] = menu.split('-');
+  return { name, quantity: Number(quantity) };
+}
+
+function isDrinkOnlyOrder(orderMenus) {
+  const isDrinkOnly = orderMenus.every((menu) => {
+    const { name } = transformOrderMenuFormat(menu);
+    return Object.keys(Event.menu.dessert).includes(name);
+  });
+
+  return isDrinkOnly || orderMenus.length < 0;
+}
+
+function isInvalidQuantity(quantity) {
+  return Number.isNaN(quantity) || quantity < 0;
+}
+
+function isOrderLimitOver(orderList, quantity) {
+  const totalOrders = Object.values(orderList).reduce(
+    (total, currentQuantity) => total + currentQuantity,
+    0,
+  );
+  return totalOrders + quantity > 20;
 }
 
 export function validateMenu(orderMenus) {
-  if (!orderMenus) throwValidationError('유효하지 않은 주문입니다.');
-
-  // 음료만 주문한 경우
-  const isDrinkOnlyOrder = orderMenus.every((menu) => {
-    const [name] = menu.split('-');
-    return isValidMenuName(name, Object.keys(Event.menu.dessert));
-  });
-
-  if (isDrinkOnlyOrder && orderMenus.length > 0)
-    throwValidationError('유효하지 않은 주문입니다.');
+  if (!orderMenus || isDrinkOnlyOrder(orderMenus))
+    throwValidationError(ERROR_MESSAGE.INVALID_ORDER);
 
   const orderList = {};
-  // '-'문자가 포함되지 않은 경우
   orderMenus.forEach((menu) => {
-    if (!menu.includes('-'))
-      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+    const { name, quantity } = transformOrderMenuFormat(menu);
 
-    const [name, quantity] = menu.split('-');
-    const eventMenus = [
-      ...Object.keys(Event.menu.appetizer),
-      ...Object.keys(Event.menu.main),
-      ...Object.keys(Event.menu.dessert),
-      ...Object.keys(Event.menu.drink),
-    ];
-    // 메뉴판에 없는 메뉴일 경우
-    if (!isValidMenuName(name, eventMenus))
-      throwValidationError('유효하지 않은 주문입니다.');
-    // 주문 갯수가 1이상의 숫자가 아닐 경우
-    if (Number.isNaN(Number(quantity)) || Number(quantity) < 0)
-      throwValidationError('유효하지 않은 주문입니다.');
-    // 메뉴를 한 번에 최대 20개 초과로 주문한 경우
-    const totalOrders = Object.values(orderList).reduce(
-      (total, qty) => total + qty,
-      0,
-    );
-    if (totalOrders + Number(quantity) > 20)
-      throwValidationError('유효하지 않은 주문입니다.');
-    // 중복 메뉴를 입력한 경우
-    if (orderList[name]) throwValidationError('유효하지 않은 주문입니다.');
+    if (!isValidMenuName(name) || orderList[name])
+      throwValidationError(ERROR_MESSAGE.INVALID_ORDER);
+    if (isInvalidQuantity(quantity) || isOrderLimitOver(orderList, quantity))
+      throwValidationError(ERROR_MESSAGE.INVALID_ORDER);
 
-    orderList[name] = Number(quantity);
+    orderList[name] = quantity;
   });
 
   return orderList;
